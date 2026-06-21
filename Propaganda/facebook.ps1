00c33c7c5842f3856f3b8088ac15b895b41542e0
monouk-prog <p20240062@gmail.com>
2026-06-22 00:58:45 +0700

    update to propaganda

--- Propaganda/facebook.ps1
+++ Propaganda/facebook.ps1
@@ -82,10 +82,11 @@ $postbutton = $maxmx/2
 Start-Sleep -Seconds 3
 
 # Automated Navigation
-$wshell.SendKeys("Beautiful Day")
+$wshell.SendKeys("https://www.facebook.com/100089462335127/posts/pfbid0dJV9cdjedwg1zjQbVgRHUobfv7ZLGH2GZ6339ZbvYmRteCXut1cqhXRfi5m7BYXFl/?mibextid=wwXIfr")
 for ($i = 1; $i -le 10; $i++) {
 	$wshell.SendKeys("{TAB}")
 }
+Start-Sleep 3
 $wshell.SendKeys("{ENTER}")
 Start-Sleep 3
 for ($i = 1; $i -le 8; $i++) {
