3ed6f1a219fe834a17fc6add20adacc9aa05418e
monouk-prog <p20240062@gmail.com>
2026-06-22 08:08:50 +0700

    Update facebook.ps1

--- Propaganda/facebook.ps1
+++ Propaganda/facebook.ps1
@@ -81,7 +81,7 @@ Start-Sleep -Seconds 3
 
 # Automated Navigation
 $wshell.SendKeys("https://www.facebook.com/100089462335127/posts/pfbid0dJV9cdjedwg1zjQbVgRHUobfv7ZLGH2GZ6339ZbvYmRteCXut1cqhXRfi5m7BYXFl/?mibextid=wwXIfr")
-Start-Sleep 3
+Start-Sleep 6
 for ($i = 1; $i -le 17; $i++) {
 	$wshell.SendKeys("{TAB}")
 }
